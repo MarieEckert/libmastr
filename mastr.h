03e286e92374d1eea103fcdaeb51bdbca901a808
Marie <marie@ha.si>
2025-03-24 12:17:17 +0100

    add documentation to existing functions

--- mastr.h
+++ mastr.h
@@ -41,22 +41,48 @@
 	MASTR_PP_STRINGIFY(LIBMASTR_VERSION_MAJOR) \
 	"." MASTR_PP_STRINGIFY(LIBMASTR_VERSION_MINOR)
 
+/**
+ * @brief Utility macro for constructing a heap-allocated String
+ * from a string-literal.
+ */
+#define MASTR_STRING(str) mastr_string_from_cstr(str)
+
 typedef struct {
 	uint32_t capacity;
 	uint32_t length;
 	char data[];
 } String;
 
+/**
+ * @brief Create a new String with the given byte capacity.
+ */
 String *mastr_string_new(uint32_t byteCapacity);
 
+/**
+ * @brief Constructs a String with the contents of the given String.
+ */
 String *mastr_string_from(const String *original);
 
+/**
+ * @brief Constructs a String with the contents of the given C-String.
+ */
 String *mastr_string_from_cstr(const char *original);
 
+/**
+ * @brief Appends the contents of String b to String a.
+ */
 String *mastr_string_append(String *a, const String *b);
 
+/**
+ * @brief Appends the contents of C-String b to String a.
+ */
 String *mastr_string_append_cstr(String *a, const char *b);
 
+/**
+ * @brief Resize the given string so that its capacity is equal to
+ * or greater than newSize. The newSize will be aligned to the value
+ * of the MASTR_RESIZE_ALIGNMENT preprocessor macro.
+ */
 String *mastr_string_resize(String *a, uint32_t newSize);
 
 /* refcounted strings */
@@ -89,6 +115,12 @@ String *mastr_string_resize(String *a, uint32_t newSize);
 	}
 
 /**
+ * @brief Utility macro to construct a RCString with heap allocated data
+ * from a string literal.
+ */
+#define MASTR_RCSTRING(str) mastr_rcstring_from_cstr(str)
+
+/**
  * @brief Container Type for a String for enabling reference-counting.
  * To properly utilise this call the MASTR_RCSTRING_USE macro with the RCString
  * as its parameter at the start of the scope where it is used and call the
@@ -114,12 +146,26 @@ typedef struct {
  */
 RCStringResult mastr_rcstring_new(uint32_t byteCapacity);
 
+/**
+ * @brief Constructs a RCString with the contents of the given String.
+ */
 RCStringResult mastr_rcstring_from(const String *original);
 
+/**
+ * @brief Constructs a RCString with the contents of the given C-String.
+ */
 RCStringResult mastr_rcstring_from_cstr(const char *original);
 
+/**
+ * @brief Appends the contents of RCString b to RCString a.
+ * @note This function does not affect the reference count of its parameters.
+ */
 RCStringResult mastr_rcstring_append(RCString a, RCString b);
 
+/**
+ * @brief Appends the contents of C-String b to RCString a.
+ * @note This function does not affect the reference count of its parameters.
+ */
 RCStringResult mastr_rcstring_append_cstr(RCString a, const char *b);
 
 #endif
