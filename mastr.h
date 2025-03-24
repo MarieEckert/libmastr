4f8b9db9261d5552b19e1eef574dff529d93229d
Marie <marie@ha.si>
2025-03-24 12:20:53 +0100

    relicense

--- mastr.h
+++ mastr.h
@@ -9,20 +9,9 @@
  * standard.
  *
  * ============================================================================
- * Copyright (C) 2024  Marie Eckert
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (C) 2025, Marie Eckert
+ * Licensed under the BSD 3-Clause License.
  */
 
 #ifndef ME_LIBMASTR_H
@@ -40,6 +29,8 @@
 #define MASTR_VERSION_STRING                   \
 	MASTR_PP_STRINGIFY(LIBMASTR_VERSION_MAJOR) \
 	"." MASTR_PP_STRINGIFY(LIBMASTR_VERSION_MINOR)
+
+/* Regular Dynamic String */
 
 /**
  * @brief Utility macro for constructing a heap-allocated String
