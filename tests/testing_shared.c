cab2ee63563ce5786fdf37c02ef6d3d004649481
Marie <marie@ha.si>
2025-04-03 21:48:13 +0200

    add shared testing code

---
+++ tests/testing_shared.c
@@ -0,0 +1,19 @@
+int current_step = 1;
+
+#define BEGIN_STEP(msg)                                              \
+	do {                                                             \
+		fprintf(stderr, "\t[%d/%d] " msg, current_step, TEST_STEPS); \
+		current_step++;                                              \
+	} while(0)
+
+#define STEP_LOG_PRIMER "\t    -> "
+
+#define STEP_SUCCESS                                     \
+	do {                                                 \
+		fprintf(stderr, "  \x1b[32;1mSUCCESS\x1b[0m\n"); \
+	} while(0)
+
+#define STEP_FAIL                                       \
+	do {                                                \
+		fprintf(stderr, "  \x1b[31;1mFAILED\x1b[0m\n"); \
+	} while(0)
